efdd1eb948d1f71fa6a74154cc7744fe276cdc96
EliteOneTube <manos1234599@gmail.com>
2024-01-17 22:07:58 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,26 @@
+from celery import Celery, Task
+from flask import Flask
+
+def celery_init_app(app: Flask) -> Celery:
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+    return celery_app
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config.from_mapping(
+        CELERY=dict(
+            broker_url="amqp://celery-app:testing@localhost:5672/celery",
+            result_backend="mongodb://localhost:27017/celery_example"
+        ),
+    )
+    app.config.from_prefixed_env()
+    celery_init_app(app)
+    return app
